558ac3782d9a4e84a81c35d4471c4bea4d2722e5
Nad33mM <37762590+Nad33mM@users.noreply.github.com>
2018-03-26 18:21:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express'),
     app = express(),
     server = require('http').Server(app),
-    io = require('socket.io')(server);
+    io = require('socket.io').listen(server);
     
 Object.assign=require('object-assign')
 
